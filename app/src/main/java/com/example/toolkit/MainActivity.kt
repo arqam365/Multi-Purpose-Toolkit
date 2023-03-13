8cee0d4914b01ea316f5c4e0aa705fe7b6bc506a
Arqam Ahmad Siddiqui <arqamahmad365.au@gmail.com>
2023-03-13 09:45:46 +0530

    Updates 1.3

--- app/src/main/java/com/example/toolkit/MainActivity.kt
+++ app/src/main/java/com/example/toolkit/MainActivity.kt
@@ -1,11 +1,9 @@
-package com.example.toolkit
+package com.nextlevelprogrammers.toolkit
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
