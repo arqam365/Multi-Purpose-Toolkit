702bffbf20ffea8bc4eea27d1f99aa5b5faea666
Arqam Ahmad Siddiqui <arqamahmad365.au@gmail.com>
2023-04-05 05:45:32 +0530

    Updates 1.3.2

--- app/src/test/java/com/example/toolkit/ExampleUnitTest.kt
+++ app/src/test/java/com/example/toolkit/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.toolkit
 import org.junit.jupiter.api.Test
 
 import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +13,6 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        Assertions.assertEquals(4, 2 + 2)
     }
 }
